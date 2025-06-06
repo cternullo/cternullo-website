0d89a91f6195919b2769ec688118595c7ca20aa0
cternullo <112415035+cternullo@users.noreply.github.com>
2025-06-06 16:51:53 +0100

    Initial commit for new repository

--- src/components/ParallaxSection.tsx
+++ src/components/ParallaxSection.tsx
@@ -1,13 +1,14 @@
 import { useParallax } from 'react-scroll-parallax';
+import { type RefObject } from 'react';
 
 const ParallaxSection = () => {
-  const parallax = useParallax({
+  const { ref } = useParallax<HTMLDivElement>({
     speed: -10,
   });
 
   return (
     <div className="min-h-screen relative">
-      <div ref={parallax.ref} className="absolute inset-0">
+      <div ref={ref as RefObject<HTMLDivElement>} className="absolute inset-0">
         {/* Your content here */}
         <h1 className="text-4xl text-white text-center pt-20">Welcome to My Portfolio</h1>
       </div>
