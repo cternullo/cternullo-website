0dec1049fb2d40325080716f4f4bdacdb219707e
cternullo <112415035+cternullo@users.noreply.github.com>
2025-06-06 17:37:39 +0100

    Rebuild with source files

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/cternullo-website/',  // Updated to match new repository name
-})
